c47b1ba6a6985b2660701ddaba79d4d254b86de6
gmaulana-alt <140475525+gmaulana-alt@users.noreply.github.com>
2026-09-16 08:52:25 +0700

    Add files via upload

---
+++ styles/TanahdiSemeru88dariBhumi_1_style.js
@@ -0,0 +1,53 @@
+var size = 0;
+var placement = 'point';
+function categories_TanahdiSemeru88dariBhumi_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement, textAlign, offsetX, offsetY, overflow, repeat) {
+    var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+    switch(valueStr) {
+        case '1':
+            return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(100,152,210,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(165,191,221,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+			break;
+default:
+            return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(247,247,247,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(150,150,150,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+			break;
+    }};
+
+var style_TanahdiSemeru88dariBhumi_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("Yes/No");
+    var labelFont = "9.1px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = true;
+    var repeat = 0;
+    var placement = 'point';
+    if (feature.get("Nomor") !== null) {
+        labelText = String(feature.get("Nomor"));
+    }
+    
+    var style = categories_TanahdiSemeru88dariBhumi_1(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement, textAlign, offsetX, offsetY, overflow, repeat);
+
+    return style;
+};
